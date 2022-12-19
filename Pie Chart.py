437581764e5a1a6c359e7c9de4e6106f4a3e889f
Ronit Bhatt <ronitbhatt360@gmail.com>
2022-12-19 18:51:39 +0530

    Result

---
+++ Pie Chart.py
@@ -0,0 +1,11 @@
+#Result in the form of a piechart
+mylabels=["Positive","Negative","Neutral"]
+#pos=No. of Positives
+#neg=No. of Negatives
+#new=No. of neutrals
+x=[pos,neg,new]
+labels = list(mylabels)
+myexplode = [0.2, 0.2, 0]
+colors = ['orange',  'darkblue', 'red']# choosing colours
+plt.pie(x,labels=labels,colors=colors,explode=myexplode,autopct='%1.1f%%', shadow=True, startangle=90)
+plt.show()
