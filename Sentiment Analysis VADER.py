2ea84d802020f1563b7917f0560e0f1051f5e980
Ronit Bhatt <ronitbhatt360@gmail.com>
2022-12-19 18:40:16 +0530

    Create Sentiment Analysis VADER.py

---
+++ Sentiment Analysis VADER.py
@@ -0,0 +1,18 @@
+#VADER : Valence Aware Dictionary and sEntiment Reasoner
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+data1["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data1["Comment"]]
+data1["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data1["Comment"]]
+data1["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data1["Comment"]]
+data1['Compound'] = [sentiments.polarity_scores(i)["compound"] for i in data1["Comment"]]
+score = data1["Compound"].values
+sentiment = []
+for i in score:
+    if i >= 0.05 :
+        sentiment.append('Positive')
+    elif i <= -0.05 :
+        sentiment.append('Negative')
+    else:
+        sentiment.append('Neutral')
+data1["Sentiment"] = sentiment
+data1.head()
